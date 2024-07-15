c4d7d12b59b2940688a777368bf903770717ecfe
asimkhada1123 <anujsimkhada007@gmail.com>
2024-07-15 11:35:33 -0500

    Helshenki: Adding classes for Part 8

---
+++ Demo/src/Helshenki/Part8/Part8.java
@@ -0,0 +1,14 @@
+package Helshenki.Part8;
+
+public class Part8 {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World  ");
+
+
+
+    }
+}
---
+++ Demo/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World  ");
+
+
+
+    }
+
+}
